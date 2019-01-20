b50aedd186a45a083e0dc59a7c84187dc3e5649a
Ben <benjamin.slabbert@gmail.com>
2019-01-20 11:28:39 +0200

    adding proto for getting latest products

--- src/js/grpc/proto_grpc_pb.js
+++ src/js/grpc/proto_grpc_pb.js
@@ -4,41 +4,41 @@
 var grpc = require('grpc');
 var src_main_proto_proto_pb = require('../proto/proto_pb.js');
 
-function serialize_io_github_benslabbert_trak_grpc_HelloRequest(arg) {
-    if (!(arg instanceof src_main_proto_proto_pb.HelloRequest)) {
-        throw new Error('Expected argument of type io.github.benslabbert.trak.grpc.HelloRequest');
+function serialize_io_github_benslabbert_trak_grpc_Empty(arg) {
+    if (!(arg instanceof src_main_proto_proto_pb.Empty)) {
+        throw new Error('Expected argument of type io.github.benslabbert.trak.grpc.Empty');
     }
     return new Buffer(arg.serializeBinary());
 }
 
-function deserialize_io_github_benslabbert_trak_grpc_HelloRequest(buffer_arg) {
-    return src_main_proto_proto_pb.HelloRequest.deserializeBinary(new Uint8Array(buffer_arg));
+function deserialize_io_github_benslabbert_trak_grpc_Empty(buffer_arg) {
+    return src_main_proto_proto_pb.Empty.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_io_github_benslabbert_trak_grpc_HelloResponse(arg) {
-    if (!(arg instanceof src_main_proto_proto_pb.HelloResponse)) {
-        throw new Error('Expected argument of type io.github.benslabbert.trak.grpc.HelloResponse');
+function serialize_io_github_benslabbert_trak_grpc_LatestResponse(arg) {
+    if (!(arg instanceof src_main_proto_proto_pb.LatestResponse)) {
+        throw new Error('Expected argument of type io.github.benslabbert.trak.grpc.LatestResponse');
     }
     return new Buffer(arg.serializeBinary());
 }
 
-function deserialize_io_github_benslabbert_trak_grpc_HelloResponse(buffer_arg) {
-    return src_main_proto_proto_pb.HelloResponse.deserializeBinary(new Uint8Array(buffer_arg));
+function deserialize_io_github_benslabbert_trak_grpc_LatestResponse(buffer_arg) {
+    return src_main_proto_proto_pb.LatestResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 
-var HelloServiceService = exports.HelloServiceService = {
-    hello: {
-        path: '/io.github.benslabbert.trak.grpc.HelloService/hello',
+var LatestServiceService = exports.LatestServiceService = {
+    latest: {
+        path: '/io.github.benslabbert.trak.grpc.LatestService/latest',
         requestStream: false,
         responseStream: false,
-        requestType: src_main_proto_proto_pb.HelloRequest,
-        responseType: src_main_proto_proto_pb.HelloResponse,
-        requestSerialize: serialize_io_github_benslabbert_trak_grpc_HelloRequest,
-        requestDeserialize: deserialize_io_github_benslabbert_trak_grpc_HelloRequest,
-        responseSerialize: serialize_io_github_benslabbert_trak_grpc_HelloResponse,
-        responseDeserialize: deserialize_io_github_benslabbert_trak_grpc_HelloResponse,
+        requestType: src_main_proto_proto_pb.Empty,
+        responseType: src_main_proto_proto_pb.LatestResponse,
+        requestSerialize: serialize_io_github_benslabbert_trak_grpc_Empty,
+        requestDeserialize: deserialize_io_github_benslabbert_trak_grpc_Empty,
+        responseSerialize: serialize_io_github_benslabbert_trak_grpc_LatestResponse,
+        responseDeserialize: deserialize_io_github_benslabbert_trak_grpc_LatestResponse,
     },
 };
 
-exports.HelloServiceClient = grpc.makeGenericClientConstructor(HelloServiceService);
+exports.LatestServiceClient = grpc.makeGenericClientConstructor(LatestServiceService);
