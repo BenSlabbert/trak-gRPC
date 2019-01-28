7b02e6c35fd6b485192eb45dfe3ff7e6772a4e94
Ben <benjamin.slabbert@gmail.com>
2019-01-28 17:00:45 +0200

    updating search proto

--- src/js/grpc/proto_grpc_pb.js
+++ src/js/grpc/proto_grpc_pb.js
@@ -48,7 +48,44 @@ function deserialize_io_github_benslabbert_trak_grpc_ProductResponse(buffer_arg)
     return src_main_proto_proto_pb.ProductResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_io_github_benslabbert_trak_grpc_SearchRequest(arg) {
+    if (!(arg instanceof src_main_proto_proto_pb.SearchRequest)) {
+        throw new Error('Expected argument of type io.github.benslabbert.trak.grpc.SearchRequest');
+    }
+    return new Buffer(arg.serializeBinary());
+}
 
+function deserialize_io_github_benslabbert_trak_grpc_SearchRequest(buffer_arg) {
+    return src_main_proto_proto_pb.SearchRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_io_github_benslabbert_trak_grpc_SearchResponse(arg) {
+    if (!(arg instanceof src_main_proto_proto_pb.SearchResponse)) {
+        throw new Error('Expected argument of type io.github.benslabbert.trak.grpc.SearchResponse');
+    }
+    return new Buffer(arg.serializeBinary());
+}
+
+function deserialize_io_github_benslabbert_trak_grpc_SearchResponse(buffer_arg) {
+    return src_main_proto_proto_pb.SearchResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+var SearchServiceService = exports.SearchServiceService = {
+    search: {
+        path: '/io.github.benslabbert.trak.grpc.SearchService/search',
+        requestStream: false,
+        responseStream: false,
+        requestType: src_main_proto_proto_pb.SearchRequest,
+        responseType: src_main_proto_proto_pb.SearchResponse,
+        requestSerialize: serialize_io_github_benslabbert_trak_grpc_SearchRequest,
+        requestDeserialize: deserialize_io_github_benslabbert_trak_grpc_SearchRequest,
+        responseSerialize: serialize_io_github_benslabbert_trak_grpc_SearchResponse,
+        responseDeserialize: deserialize_io_github_benslabbert_trak_grpc_SearchResponse,
+    },
+};
+
+exports.SearchServiceClient = grpc.makeGenericClientConstructor(SearchServiceService);
 var LatestServiceService = exports.LatestServiceService = {
     latest: {
         path: '/io.github.benslabbert.trak.grpc.LatestService/latest',
